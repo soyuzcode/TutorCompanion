f5fc27fb1b9d0b07e88b2f28c7e7aa7dbb2ae7a0
charleswiiflowers <carlitosfg.5051@gmail.com>
2026-04-06 10:50:29 -0600

    [🐳] Commit: Add Demo UI

--- source/main.py
+++ source/main.py
@@ -1,8 +1,16 @@
 """Here we're gonna connect every module to UI"""
-
+from ui import TutorCompanion
 
 class Main():
-    """Everything here"""
+    def __init__(self) -> None:
+
+
+
+        UI = TutorCompanion()
+        UI.run()
+
+        # Don't write anything here!!
+        # This will execute when kill UI
 
 
 if __name__ == "__main__":
--- source/ui.py
+++ source/ui.py
@@ -1,3 +1,9 @@
-import tkinter
+from kivy.app import App
+from kivy.uix.widget import Widget
 
-"""Hello World!!!"""
+class HelloWorld(Widget):
+    pass
+
+class TutorCompanion(App):
+    def build(self):
+        return HelloWorld()
