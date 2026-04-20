0971427b6b7896be5d7ca90d133e4e71c58a01fb
aaquino-m <aaquino.michelle@gmail.com>
2026-04-19 20:50:52 -0600

    [🔗]  Commit: Add API URL and fetch JSON data

--- App/source/modules/config.py
+++ App/source/modules/config.py
@@ -1,1 +1,5 @@
 """File to manage user settings"""
+
+BASE_URL = "https://tutorcompanionv1.onrender.com/get/users/"
+
+
--- App/source/modules/get_data.py
+++ App/source/modules/get_data.py
@@ -1,1 +1,30 @@
 """In this file we're gonna get data from an API hosted by Vercel"""
+
+import requests 
+from modules.config import BASE_URL
+
+def get_json_data():
+    """Fetches JSON from the API and returns it as a Python dictionary"""
+
+    try:
+        response = requests.get(BASE_URL)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error: Received status code {response.status_code}")
+            return None
+        
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+if __name__ == "__main__":
+    print('Pronando conexion con API')
+    data = get_json_data()
+
+    if data:
+        print("Data received from API:")
+        print(data) 
+    else:
+        print("Failed to retrieve data from API")
+    
