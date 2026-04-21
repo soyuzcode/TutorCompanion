f8c405be3b343655b27fde18d07a0db75073e228
charleswiiflowers <carlitosfg.5051@gmail.com>
2026-04-21 09:27:32 -0600

    [🐸] Commit: Add setting

--- App/source/modules/config.py
+++ App/source/modules/config.py
@@ -3,3 +3,11 @@
 BASE_URL = "https://tutorcompanionv1.onrender.com/get/users/"
 
 
+def get_base_Url():
+    """Devuelve la URL Base"""
+
+    #
+    #
+    #
+
+    return "https://tutorcompanionv1.onrender.com/"
