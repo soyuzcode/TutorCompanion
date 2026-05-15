8ec7ea2f2629e5f106cb88c02b7f7ebdb14a5a73
charleswiiflowers <carlitosfg.5051@gmail.com>
2026-05-15 10:14:35 -0600

    [🎃] Commit: Add UserController

--- Server/TutorCompanion/src/main/java/com/keycode/TutorCompanion/controller/UserController.java
+++ Server/TutorCompanion/src/main/java/com/keycode/TutorCompanion/controller/UserController.java
@@ -1,12 +1,29 @@
 package com.keycode.TutorCompanion.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.keycode.TutorCompanion.model.User;
+import com.keycode.TutorCompanion.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 @RestController
-@RequestMapping("/get")
+@RequestMapping("/user")
 public class UserController {
 
-    
+    private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping({"", "/"})
+    public ResponseEntity<List<User>> getAllUser(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
 }
