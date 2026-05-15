32fa34302cee03f4249e172149d16fd424cd24f1
charleswiiflowers <carlitosfg.5051@gmail.com>
2026-05-15 11:50:41 -0600

    [🍩] Commit: Update UI

--- App/source/ui.py
+++ App/source/ui.py
@@ -4,17 +4,12 @@ from kivy.uix.widget import Widget
 class HelloWorld(Widget):
     pass
 
-
 class TutorCompanion(MDApp):
 
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Olive"
+        self.theme_cls.primary_palette = "Green"
         return HelloWorld()
 
     def imprimir_datos(self, rol, nombre):
         print(f"Rol seleccionado: {rol} - Usuario: {nombre}")
-
-
-if __name__ == "__main__":
-    TutorCompanion().run()
