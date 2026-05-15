e69f5695a6248b6f603a65d7f26d4b8ffe63c8cf
charleswiiflowers <carlitosfg.5051@gmail.com>
2026-05-15 13:18:36 -0600

    [🐮] Feature: Add BASE_URL to CONFIG

--- App/source/modules/config.py
+++ App/source/modules/config.py
@@ -1,16 +1,15 @@
 """File to manage user settings"""
 import json
 
-BASE_URL = "https://tutorcompanionv1.onrender.com/get/users/"
+BASE_URL = "https://tutorcompanionv1.onrender.com/"
 
 
 def get_base_Url():
     """Devuelve la URL Base"""
 
     # with open tal tal tal as file
-    file = ""
-
-    setting = json.load(file)
+    with open("Data/settings.json", "r+") as file:
+        setting = json.load(file)
 
     return setting["BASE_URL"]
 ## recomendar funciones para settings (json)
