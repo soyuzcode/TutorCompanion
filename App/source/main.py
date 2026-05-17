d30f1ed3e84419c33da13a1a84889470d7b83a24
charleswiiflowers <carlitosfg.5051@gmail.com>
2026-05-16 21:57:37 -0600

    [🎈] Feature: Add Login backend

--- App/source/main.py
+++ App/source/main.py
@@ -1,12 +1,13 @@
 """Here we're gonna connect every module to UI"""
 from ui import TutorCompanion
 from modules.process_data import is_psk_and_username_valid
+from modules.get_data import get_user_data
 
 class Main():
     def __init__(self) -> None:
 
         UI = TutorCompanion(
-            check_login=is_psk_and_username_valid)
+            check_login=is_psk_and_username_valid, data=get_user_data)
         UI.run()
 
         # Don't write anything here!!
--- App/source/ui.py
+++ App/source/ui.py
@@ -6,16 +6,25 @@ class HelloWorld(MDScreen):
 
 class TutorCompanion(MDApp):
     def __init__(
-            self, check_login,**kwargs):
+            self,
+            check_login,
+            data, **kwargs):
         
         super().__init__(**kwargs)
 
         self.check_login = check_login
+        self.get_user_data = data
 
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Cyan"
         return HelloWorld()
 
-    def imprimir_datos(self, rol, nombre):
-        print(f"Rol seleccionado: {rol} - Usuario: {nombre}")
+    def on_login(self, user, psk):
+        if self.check_login(user=user, psk=psk, data=self.get_user_data()):
+            
+            print("Alright! You're good!")
+        
+        else:
+            
+            print("Wrong!")
