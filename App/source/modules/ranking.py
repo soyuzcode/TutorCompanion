b03da5b446ca3f5feb1259f8b99d146b062c0317
Miguel-manzanares <miguelmanzanares2008@gmail.com>
2026-05-19 08:22:29 -0600

    Update_Ranking

---
+++ App/source/modules/ranking.py
@@ -0,0 +1,23 @@
+def get_featured_tutors(data):
+
+    tutores = []
+
+    for persona in data:
+
+        # Verificar si es tutor
+        if persona["tutorProfile"] != None:
+
+            nombre = persona["name"]
+            puntaje = persona["tutorProfile"]["rating"]
+
+            tutor = {
+                "name": nombre,
+                "rating": puntaje
+            }
+
+            tutores.append(tutor)
+
+    # Ordenar del mayor al menor puntaje
+    tutores.sort(key=lambda x: x["rating"], reverse=True)
+
+    return tutores
